b4b14914fd14053cde919a5bf5c23471c8c45fda
chaseMengdi <1018952520@qq.com>
2018-04-08 11:14:53 +0800

    complete stage2

--- src/wcPro/wcPro.java
+++ src/wcPro/wcPro.java
@@ -2,7 +2,7 @@ package wcPro;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collections; 
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
